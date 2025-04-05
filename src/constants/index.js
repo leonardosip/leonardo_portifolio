90ecbb1449f10ae1aaf926e339c6b1730dba0e53
leonardosip <leonarddopereira2004@gmail.com>
2025-04-05 14:31:00 -0300

    configurando logos

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,20 +3,15 @@ import {
     backend,
     web,
     javascript,
-    typescript,
     html,
     css,
     reactjs,
-    redux,
     tailwind,
     nodejs,
-    mongodb,
     git,
     figma,
-    docker,
     cohab,
     click,
-    carrent,
     jobit,
     tripguide,
     threejs,
@@ -141,27 +136,6 @@ import {
   ];
   
   const projects = [
-    {
-      name: "Car Rent",
-      description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
     {
       name: "Job IT",
       description:
